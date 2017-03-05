3d76b426abde6ebae103c871581878bba286e2b8
binye33333 <binye4021>
2017-03-05 12:28:21 +0800

    测试一下快速排序法

--- app/src/main/java/JavaTest/Test.java
+++ app/src/main/java/JavaTest/Test.java
@@ -1,5 +1,7 @@
 package JavaTest;
 
+import java.util.Arrays;
+
 /**
  * Created by chenyou729 on 16/12/20.
  */
@@ -8,9 +10,12 @@ public class Test {
 
 
     public static void main(String[] args) {
-        System.out.println("hello:"+calculateRabit(5));
-        printSuNumber(100,200) ;
-        revertPoint() ;
+        int[] s= {14,1,11,4,7,55,22,12,11,18,3,88,71} ;
+        quick_sort(s,0,s.length-1) ;
+        System.out.println(Arrays.toString(s));
+//        System.out.println("hello:"+calculateRabit(5));
+//        printSuNumber(100,200) ;
+//        revertPoint() ;
     }
 
     /**
@@ -88,4 +93,29 @@ public class Test {
         }
 
     }
+
+
+    //快速排序
+    public static void quick_sort(int[] s, int l, int r)
+    {
+        if (l < r)
+        {
+            //Swap(s[l], s[(l + r) / 2]); //将中间的这个数和第一个数交换 参见注1
+            int i = l, j = r, x = s[l];
+            while (i < j)
+            {
+                while(i < j && s[j] >= x) // 从右向左找第一个小于x的数
+                    j--;
+                    s[i] = s[j];
+
+                while(i < j && s[i] < x) // 从左向右找第一个大于等于x的数
+                    i++;
+                    s[j] = s[i];
+            }
+            s[i] = x;
+            quick_sort(s, l, i - 1); // 递归调用
+            quick_sort(s, i + 1, r);
+        }
+    }
+
 }
